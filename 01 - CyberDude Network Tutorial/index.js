7c0d53899a73cdb91772c688f7ca434693707ffd
DhineshDV3 <dhineshdv3@gmail.com>
2022-09-05 18:17:00 +0530

    Javascript variables

--- 01 - CyberDude Network Tutorial/index.js
+++ 01 - CyberDude Network Tutorial/index.js
@@ -1,10 +1,15 @@
-// Echoing JavaScript
-alert("Hello JavaScript");
+var user_name = "Dhinesh";
+var msg = "Hello I'm " + user_name + ". I am building JS!"
 
-// Arithmetic
-alert(4 + 5);
+// Alerting
+alert(msg);
+// Logging
+console.log(msg);
 
-/*
-Console Logging
-*/
-console.log("Hello I'm building JS!");
+/*----------------------------------------------*/
+let number_1 = 4;
+let number_2 = 12;
+
+let result = number_1 + number_2;
+alert(result);
+console.log(result);
