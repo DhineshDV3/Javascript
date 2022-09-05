21d41129cb8b909084181eb7949680c1ba8dd939
DhineshDV3 <dhineshdv3@gmail.com>
2022-09-05 19:45:10 +0530

    Javascript Datatypes

---
+++ 01 - CyberDude Network Tutorial/01 - datatypes/index.js
@@ -0,0 +1,6 @@
+let firstName   =   "Dhinesh"   //  String
+let age         =   24;         //  Integer/Number
+let isYoung     =   true;       //  Boolean
+let lastName    =   undefined;  //  Undefined
+let last                        //  Undefined
+let trophy      =   null;       //  null
