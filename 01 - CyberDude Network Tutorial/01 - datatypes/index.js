066588a1c4855634a0ac34cd86bd1a80660641d2
DhineshDV3 <dhineshdv3@gmail.com>
2022-09-05 20:09:27 +0530

    Javascript Datatypes - Objects

--- 01 - CyberDude Network Tutorial/01 - datatypes/index.js
+++ 01 - CyberDude Network Tutorial/01 - datatypes/index.js
@@ -4,3 +4,30 @@ let isYoung     =   true;       //  Boolean
 let lastName    =   undefined;  //  Undefined
 let last                        //  Undefined
 let trophy      =   null;       //  null
+
+/*--------------Objects--------------------------*/
+
+// let user_name = "Dhinesh";
+// let user_age = 24;
+// let user_gender = "Male";
+// let user_address = "Salem, Tamil Nadu, India";
+
+let person = {
+    user_name:"Dhinesh",
+    user_age:24,
+    user_gender:"Male",
+    user_address:"Salem, Tamil Nadu, India",
+    siblings:{
+        brother:"Dhanush",
+        sister:"Anu Rekha"
+    }
+};
+// Object
+console.log(person);
+
+// Dot Notation => Mostly Used
+console.log(person.user_name);
+console.log(person.siblings.brother);
+
+// Bracket Notation
+console.log(person[user_name]);
