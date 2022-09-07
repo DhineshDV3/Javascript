fd6c16218125c82888f65a0fc1bbad6c8306d6c4
DhineshDV3 <dhineshdv3@gmail.com>
2022-09-07 13:10:35 +0530

    JavaScript Datatypes - Functions

--- 01 - CyberDude Network Tutorial/01 - datatypes/index.js
+++ 01 - CyberDude Network Tutorial/01 - datatypes/index.js
@@ -1,3 +1,5 @@
+/*--------------DataTypes---------------------------------------------------------------------*/
+
 let firstName   =   "Dhinesh"   //  String
 let age         =   24;         //  Integer/Number
 let isYoung     =   true;       //  Boolean
@@ -5,7 +7,7 @@ let lastName    =   undefined;  //  Undefined
 let last                        //  Undefined
 let trophy      =   null;       //  null
 
-/*--------------Objects--------------------------*/
+/*--------------Objects-----------------------------------------------------------------------*/
 
 // let user_name = "Dhinesh";
 // let user_age = 24;
@@ -32,7 +34,7 @@ console.log(person.siblings.sister);
 // // Bracket Notation
 // console.log(person[user_name]);
 
-/*--------------Arrays---------------------------*/
+/*--------------Arrays------------------------------------------------------------------------*/
 
 let array       = [];     // Empty Array
 let favColors   = ["red", "green", "yellow", "white"];
@@ -44,3 +46,62 @@ console.log(favColors[0]);  // Display the value in 0 element
 favColors[5] = "black";     // Add the value to 5th element of the Array
 console.log(favColors);
 console.log(favColors.length);  // No. of elements present in the array
+
+/*--------------Functions---------------------------------------------------------------------*/
+
+/*--------------Functions - without Parameter-------------------------*/
+
+// To Declare a function
+
+function name() {
+    // Set of Statements
+}
+
+// To call a functions
+
+name()
+
+// Example
+function greetUser() {
+    let name = "Dhanush";
+    let msg = "Hello " + name + "!!!";
+    console.log(msg);
+}
+
+greetUser()
+
+/*--------------Functions - with Parameter-------------------------*/
+
+/* To Declare a function
+
+function name(params) {
+    Set of Statements
+}
+
+// To call a functions
+
+name(params)
+*/
+
+// Example - single parameter
+function greetUser(name) {
+    let message = "Hello " + name + "!";
+    console.log(message);
+}
+
+greetUser("Dhinesh"); 
+greetUser("Dhanush") 
+
+// Example - multiple parameter
+function greetUser(first_name, last_name) {
+    let message = "Hello " + first_name +" " + last_name + "!";
+    console.log(message);
+}
+
+function add(number1, number2) {
+    console.log(number1 + number2);
+}
+greetUser("Dhinesh", "DV3"); 
+add(12,35)
+
+/*--------------------------------------------------------------------------------------------*/
