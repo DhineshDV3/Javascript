2185d2b5488fd3812edf063de22b8171da9912ae
DhineshDV3 <dhineshdv3@gmail.com>
2022-09-07 18:37:10 +0530

    Javascript Operator - Assignment Operator

---
+++ 01 - CyberDude Network Tutorial/02 - Operators/02 - Assignment Operators/index.js
@@ -0,0 +1,37 @@
+let x = 5;
+console.log("x => ",x);
+// x++  =>  x=x+1   => 6
+
+x = x + 2   // => 7 
+console.log("x+=2 =>",x);
+
+/*-------------------------------------------------------*/
+
+//      Alter   -   Addition
+
+// x = x + 2
+x += 2
+console.log("x+=2 =>",x);
+// x = x + 5
+x += 5
+console.log("x+=5 =>",x);
+//      Alter   -   Subtraction
+
+// x = x - 2
+x -= 2
+console.log("x-=2 =>",x);
+//      Alter   -   Multiplication
+
+// x = x * 2
+x *= 2
+console.log("x*=2 =>",x);
+//      Alter   -   Division
+
+// x = x / 2
+x /= 2
+console.log("x/=2 =>",x);
+//      Alter   -   Remainder
+
+// x = x % 2
+x %= 2
+console.log("x%=2 =>",x);
